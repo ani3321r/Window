a1357c9661f9172e258dfbf7a62560422e70c5b2
ani3321r <ani3321r@gmail.com>
2025-03-28 22:41:22 +0530

    started creating some tests

--- Window/src/tests/Test.h
+++ Window/src/tests/Test.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <functional>
+#include <vector>
+#include <string>
+
 namespace test {
 	class Test {
 	public:
@@ -9,5 +13,19 @@ namespace test {
 		virtual void OnUpdate(float deltaTime) {}
 		virtual void OnRender() {}
 		virtual void OnImGuiRender() {}
+	};
+
+	class TestMenu : public Test {
+	public:
+		TestMenu(Test*& currentTestPointer);
+		void OnImGuiRender() override;
+		template<typename T>
+		void RegisterTest(const std::string& name) {
+			std::cout << "Registering test " << name << std::endl;
+			m_Tests.push_back(std::make_pair(name, []() { return new T(); }));
+		}
+	private:
+		Test*& m_CurrentTest;
+		std::vector<std::pair<std::string, std::_Function_args<Test* ()>>> m_Tests;
 	};
 }
